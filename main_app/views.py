abd54d843bce955e485146996c1811714f0dcce5
Zee <zeeboo696@gmail.com>
2024-08-14 16:10:34 -0400

    rendering Data in Templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,4 @@
-# main_app/views.py
-
 from django.shortcuts import render
-
-# Import HttpResponse to send text-based responses
-from django.http import HttpResponse
-
-# views.py
 
 class Cat:
     def __init__(self, name, breed, description, age):
@@ -26,7 +19,7 @@ cats = [
 # Define the home view function
 def home(request):
     # Send a simple HTML response
-    return HttpResponse('<h1>Hello ᓚᘏᗢ YUP</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
